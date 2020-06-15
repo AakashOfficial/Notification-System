2aca49bc92f0660b061572a145390bcd526c2f49
AakashOfficial <aakashtyagi1996@outlook.com>
2020-06-15 12:43:27 +0530

    database connection

--- models/db.js
+++ models/db.js
@@ -6,3 +6,4 @@ mongoose.connect('mongodb+srv://test_user:Password123@mongocluster-vahm9.gcp.mon
     .catch(err => console.error('Could not connect to MongoDB...', err));
 
 require('./User');
+require('./Notification');

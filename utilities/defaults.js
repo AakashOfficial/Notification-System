ebadf560fbf65968d04d3086de7a0ae953ebe886
AakashOfficial <aakashtyagi1996@outlook.com>
2020-06-15 12:47:50 +0530

    Default

---
+++ utilities/defaults.js
@@ -0,0 +1,7 @@
+exports.emailHeader = {
+    urgentHelp: 'Your Urgent Help Required',
+    eventCelebration: 'Event Celebration At Nagarro',
+    holiday: 'Holiday At Nagarro',
+    nagarroNews: 'Important News From Naarro',
+    newPolicy: 'New Policy Of Nagarro'
+}
